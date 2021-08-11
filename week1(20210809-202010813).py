952e2b65d1aed7094f3768a7b2fb8f27b56a9c58
Alanwgy <44842568+Alanwgy@users.noreply.github.com>
2021-08-11 20:30:51 +0800

    Update week1(20210809-202010813).py

--- week1(20210809-202010813).py
+++ week1(20210809-202010813).py
@@ -1,1 +1,36 @@
+‘’‘剑指offer 4 二维数组中的查找
+在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个高效的函数，输入这样的一个二维数组和一个
+整数，判断数组中是否含有该整数。 
+
+ 示例: 
+
+ 现有矩阵 matrix 如下： 
+
+ 
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+ 
+
+ 给定 target = 5，返回 true。 
+
+ 给定 target = 20，返回 false。 ‘’‘
+
+# 可以从矩阵的左下角（或者右上角）进行执行，这样子就只需要选择一个方向即可
+class Solution:
+    def findNumberIn2DArray(self, matrix: List[List[int]], target: int) -> bool:
+        i, j = len(matrix) - 1, 0
+        while i >= 0 and j < len(matrix[0]):
+            if matrix[i][j] > target:
+                i -= 1
+            elif matrix[i][j] < target:
+                j += 1
+            else:
+                return True
+        return False
+
 
